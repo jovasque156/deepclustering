45bac6910823a9584b3e9b58a479bf1946897590
Jonathan Vasquez Verdugo <jovasque156@gmail.com>
2023-02-16 14:23:41 -0500

    Improve dec class

--- deep_clustering/dec.py
+++ deep_clustering/dec.py
@@ -31,32 +31,32 @@ class DEC(nn.Module):
         else:
             self.autoencoder = autoencoder
 
-    def target_distribution_p(self, q_):
-        weight = (q_**2) / torch.sum(q_, 0)
+    def target_distribution_p(self, q):
+        weight = (q**2) / torch.sum(q, 0)
         return (weight.t()/torch.sum(weight, 1)).t().float()
 
-    def target_distribution_phi(self, phi_):
+    def target_distribution_phi(self, phi):
         #add noise to phi
         # ipdb.set_trace()
         noise = 1e-5
-        phi_hat = (phi_+noise)**(1/self.beta)
-        weight = phi_hat / torch.sum( phi_, 0).t()
+        phi_hat = (phi+noise)**(1/self.beta)
+        weight = phi_hat / torch.sum( phi, 0).t()
         return (weight.t()/torch.sum(weight, 1)).t().float()
+    
+    def project(self, x):
+        x_proj = self.autoencoder.encode(x) 
+        return x_proj
 
-    def forward(self, x):
-        # ipdb.set_trace()
-        x_proj = self.autoencoder.encode(x)
-        soft_assignment = self.clustering_layer(x_proj)
+    def soft_assignment(self, x_proj):
+        q = self.clustering_layer(x_proj)
+        return q
+    
+    def cond_prob(self, q, x_proj):
+        p = self.target_distribution_p(q)
+        matrix_p = torch.linalg.inv(torch.mm(p.t(), p))
+        centroids = torch.mm(matrix_p, torch.mm(p.t(), x_proj))
 
-        # get vector multiplication of soft_assignement and soft_assignment.t()
-        # to get the conditional probability of each group
-
-        p_ = self.target_distribution_p(soft_assignment)
-        matrix_p = torch.linalg.inv(torch.mm(p_.t(), p_))
-        centroids = torch.mm(matrix_p, torch.mm(p_.t(), x_proj))
-        
-        # fairoid_projected = self.autoencoder.encode(self.fairoid_layer.fairoid_centers).detach()
-        # cond_prob_group = self.fairoid_layer(centroids, fairoid_projected)
         cond_prob_group = self.fairoid_layer(centroids)
 
-        return (soft_assignment.float(), cond_prob_group.float(), x_proj)
+        return cond_prob_group
+
